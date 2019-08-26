06d2b91aa5afa51a13527d909b29ce73f70d9fde
LordGift <jarupath.j@cdg.co.th>
2019-08-26 15:34:25 +0700

    update discovery client

--- middleware-service/src/main/java/th/in/lordgift/service/middleware/MiddlewareController.java
+++ middleware-service/src/main/java/th/in/lordgift/service/middleware/MiddlewareController.java
@@ -1,22 +1,29 @@
 package th.in.lordgift.service.middleware;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RefreshScope
 @RestController
 public class MiddlewareController {
 
     @Value("${message}")
     private String message;
 
-//    @Autowired
-//    DiscoveryClient discoveryClient;
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @GetMapping("/")
     public MessageObject getHelloWorld() {
 
-//        List<ServiceInstance> instances = discoveryClient.getInstances("app-service");
+        List<ServiceInstance> instances = discoveryClient.getInstances("AUTH-SERVICE");
 //        instances.get(0).getUri()
 
         return new MessageObject("Hello, World!");
